1159dcf13941bf61b25da57fd5fe8b06ffda715a
babi-rmachado <babi.rodiguesmachado@gmail.com>
2026-04-30 09:45:38 -0300

    novoExemplo

--- exemplosJs/js/exemploConfirm.js
+++ exemplosJs/js/exemploConfirm.js
@@ -1,3 +1,6 @@
 function confirmar(){
     return window.confirm('Você tem certeza que deseja confirmar?')
 }
+function navConfirm(){
+    return window.confirm('Você deseja realmente ir para essa página?')
+}
