5b81cbd90eb09482ac73038c06add33bb6faaa2e
babi-rmachado <babi.rodiguesmachado@gmail.com>
2026-04-30 11:16:54 -0300

    novasAlterações

--- exemplosJs/js/exercicios.js
+++ exemplosJs/js/exercicios.js
@@ -1,3 +1,6 @@
 function excluir(){
     window.confirm('Você tem certeza que deseja excluir esse item?')
 }
+function confirmar(){
+    window.confirm('Você realmente deseja sair dessa página?')
+}
