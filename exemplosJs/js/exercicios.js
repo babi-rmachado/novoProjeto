c18b2caff927e1f3771c132f1ae8ca2fd47d5ce4
babi-rmachado <babi.rodiguesmachado@gmail.com>
2026-04-30 11:38:51 -0300

    alteraçao

--- exemplosJs/js/exercicios.js
+++ exemplosJs/js/exercicios.js
@@ -1,6 +1,9 @@
 function excluir(){
-    window.confirm('Você tem certeza que deseja excluir esse item?')
+    return window.confirm('Você tem certeza que deseja excluir esse item?')
 }
 function confirmar(){
-    window.confirm('Você realmente deseja sair dessa página?')
+    return window.confirm('Você realmente deseja sair dessa página?')
+}
+function carrinho(){
+    return window.confirm('Você tem certeza que deseja adicionar este item ao carrinho?')
 }
